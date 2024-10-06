65ada4a34275de12989400a18ddf6a401e98ef00
ch4ngg3r <141339238+ch4ngg3r@users.noreply.github.com>
2024-10-06 16:46:43 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Массив назван mas
+        private ObservableCollection<string> mas = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            vstavka.ItemsSource = mas; // Привязываем ListBox (теперь vstavka) к массиву mas
+        }
+
+        // Добавление новой записи
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(input.Text))
+            {
+                mas.Add(input.Text);
+                input.Clear();
+            }
+            else
+            {
+                MessageBox.Show("ТЫ НЕ ВЫБРАЛ ЧТО ДОБАВИТЬ");
+            }
+        }
+
+        // Удаление существующей записи
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (vstavka.SelectedItem != null)
+            {
+                mas.Remove(vstavka.SelectedItem.ToString());
+            }
+            else
+            {
+                MessageBox.Show("ТЫ НЕ ВЫБРАЛ ЧТО УДАЛИТЬ");
+            }
+        }
+
+        // Изменение существующей записи
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (vstavka.SelectedItem != null && !string.IsNullOrWhiteSpace(input.Text))
+            {
+                int selectedIndex = vstavka.SelectedIndex;
+                mas[selectedIndex] = input.Text;
+                input.Clear();
+            }
+            else
+            {
+                MessageBox.Show("ТЫ НЕ ВЫБРАЛ ЧТО ИЗМЕНИТЬ");
+            }
+        }
+    }
+}
